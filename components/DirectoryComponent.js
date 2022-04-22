51ec30c31db05ead74a0d1fa62687f279e30b8df
sanjaydobhal <sanjaydobhal@gmail.com>
2022-04-22 18:25:35 +0100

    Animatable

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -4,6 +4,7 @@ import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -17,16 +18,16 @@ class Directory extends Component {
         title: 'Directory'
     }
 
-    render(){
+    render() {
         const { navigate } = this.props.navigation;
-        const renderDirectoryItem = ({item}) => {
+        const renderDirectoryItem = ({ item }) => {
             return (
                 <Tile
                     title={item.name}
                     caption={item.description}
                     featured
                     onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-                    imageSrc={{uri: baseUrl + item.image}}
+                    imageSrc={{ uri: baseUrl + item.image }}
                 />
             );
         };
@@ -41,13 +42,15 @@ class Directory extends Component {
                 </View>
             );
         }
-        
+
         return (
-            <FlatList
-                data={this.props.campsites.campsites}
-                renderItem={renderDirectoryItem}
-                keyExtractor={item => item.id.toString()}
-            />
+            <Animatable.View animation="fadeInRightBig" duration={2000}>
+                <FlatList
+                    data={this.props.campsites.campsites}
+                    renderItem={renderDirectoryItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Animatable.View>
         );
     }
 
